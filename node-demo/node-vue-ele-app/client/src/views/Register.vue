6d78262edf40b17429662fb7c1205563699c66d6
leiyungit <744561873@qq.com>
2020-01-05 23:08:27 +0800

    register 重做

--- node-demo/node-vue-ele-app/client/src/views/Register.vue
+++ node-demo/node-vue-ele-app/client/src/views/Register.vue
@@ -1,106 +1,69 @@
 <template>
-    <div class="register">
-        <section class="form_container">
-            <div class="manage_tip">
-                <span class="title">后台管理系统</span>
-            </div>
-            <el-form :model="registerUser" :rules="rules" class="registerForm" ref="registerForm" label-width="80px">
-                <el-form-item label="用户名" prop="name">
-                    <el-input v-model="registerUser.name" placeholder="请输入用户名"></el-input>
-                </el-form-item>
-                <el-form-item label="邮箱" prop="email">
-                    <el-input v-model="registerUser.email" placeholder="请输入邮箱"></el-input>
-                </el-form-item>
-                <el-form-item label="密码" prop="password">
-                    <el-input v-model="registerUser.password" placeholder="请输入密码" type="password"></el-input>
-                </el-form-item>
-                <el-form-item label="确认密码" prop="password2">
-                    <el-input v-model="registerUser.password2" placeholder="请确认密码" type="password"></el-input>
-                </el-form-item>
-                <el-form-item label="选择身份">
-                    <el-select v-model="registerUser.identity" placeholder="请选择身份">
-                        <el-option label="管理员" value="manager"></el-option>
-                        <el-option label="员工" value="employee"></el-option>
-                    </el-select>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary"  class="submit_btn" @click="submitForm('registerForm')">注 册</el-button>
-                </el-form-item>
-            </el-form>
-        </section>
-    </div>
+  <div class="register">
+    <section class="form_container">
+      <div class="manage_tip">
+        <span class="title">后台管理系统</span>
+      </div>
+       <el-form
+        :model="registerUser"
+        status-icon
+        :rules="rules"
+        ref="registerForm"
+        label-width="100px"
+        class="registerForm"
+      >
+        <el-form-item label="用户名" prop="name">
+          <el-input v-model="registerUser.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass">
+          <el-input type="password" v-model="registerUser.pass" aria-placeholder="请输入密码" ></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="checkPass">
+          <el-input type="password" v-model="registerUser.checkPass" aria-placeholder="请输入确认密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('registerForm')">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </section>
+   
+  </div>
 </template>
 
 <script>
 export default {
   name: "register",
   data() {
-    var validatePass2 = (rule, value, callback) => {
-      if (value !== this.registerUser.password) {
-        callback(new Error("两次输入密码不一致!"));
-      } else {
-        callback();
-      }
-    };
+     var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
     return {
-      registerUser: {
-        name: "",
-        email: "",
-        password: "",
-        password2: "",
-        identity: ""
-      },
-      rules: {
-        name: [
-          { required: true, message: "用户名不能为空", trigger: "change" },
-          { min: 2, max: 30, message: "长度在 2 到 30 个字符", trigger: "blur" }
-        ],
-        email: [
-          {
-            type: "email",
-            required: true,
-            message: "邮箱格式不正确",
-            trigger: "blur"
-          }
-        ],
-        password: [
-          { required: true, message: "密码不能为空", trigger: "blur" },
-          { min: 6, max: 30, message: "长度在 6 到 30 个字符", trigger: "blur" }
-        ],
-        password2: [
-          { required: true, message: "确认密码不能为空", trigger: "blur" },
-          {
-            min: 6,
-            max: 30,
-            message: "长度在 6 到 30 个字符",
-            trigger: "blur"
-          },
-          { validator: validatePass2, trigger: "blur" }
-        ]
-      }
+       registerUser: {
+          name: '',
+          pass: '',
+          checkPass: ''
+        },
+        rules: {
+          pass: [
+            { }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+          name: [
+            { required: true, message: '用户名不能为空', trigger: "change" }
+          ]
+        }
     };
   },
   methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          console.log("submit!!");
-          // this.$axios
-          //   .post("/api/users/register", this.registerUser)
-          //   .then(res => {
-          //     // 注册成功
-          //     this.$message({
-          //       message: "注册成功！",
-          //       type: "success"
-          //     });
-          //     // this.$router.push("/login");
-          //   });
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    }
+    submitForm(formName) {}
   }
 };
 </script>
