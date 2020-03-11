bfc44b0df2111b3f454d09f661dc3eee7a2d32d7
leiyun_al <744561873@qq.com>
2020-03-11 20:46:07 +0800

    keys.js mongodb 数据2

--- node-demo/node-vue-ele-app/config/keys.js
+++ node-demo/node-vue-ele-app/config/keys.js
@@ -2,6 +2,6 @@ module.exports={
     secretOrKey:"secret",
     Bearer:"Bearer ",
     //mongoURI:"mongodb://localhost:27017/runoob"
-    //mongoURI:"mongodb+srv://test:test123456@cluster0-gzsra.azure.mongodb.net/test"
-    mongoURI:"mongodb+srv://dbapi01:dbapi01123456@cluster0-dr3tb.mongodb.net/test"
+    mongoURI:"mongodb+srv://test:test123456@cluster0-gzsra.azure.mongodb.net/test"
+    //mongoURI:"mongodb+srv://dbapi01:dbapi01123456@cluster0-dr3tb.mongodb.net/test"
 }
