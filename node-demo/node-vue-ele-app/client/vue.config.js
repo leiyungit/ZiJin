3c8aa9f8aa19df48d5af558f58ce6b49368d8f69
leiyungit <50760229+leiyungit@users.noreply.github.com>
2020-08-26 10:33:37 +0800

    Update vue.config.js
    ip访问改回localhost

--- node-demo/node-vue-ele-app/client/vue.config.js
+++ node-demo/node-vue-ele-app/client/vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
     },
     devServer: {
         open: true,
-        host: '0.0.0.0',  // localhost
+        host: 'localhost',  // localhost
         port: 8080,
         https: false,
         hotOnly: false,
